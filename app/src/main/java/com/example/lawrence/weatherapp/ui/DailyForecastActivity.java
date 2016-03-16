6169f70c928bd6cf88f99808f4a70ec16db2bc15
Lawrence L <Lawrence.Lee@alumni.ucsd.edu>
2016-03-16 00:39:13 -0700

    Add click functionality to list items in daily forecast by implementing onListItemClick. Also re-factored code to extend from Activity instead of ListActivity to learn how to handle ListViews in a regular Activity.

--- app/src/main/java/com/example/lawrence/weatherapp/ui/DailyForecastActivity.java
+++ app/src/main/java/com/example/lawrence/weatherapp/ui/DailyForecastActivity.java
@@ -1,27 +1,42 @@
 package com.example.lawrence.weatherapp.ui;
 
-import android.app.ListActivity;
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Parcelable;
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-import android.widget.ArrayAdapter;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.lawrence.weatherapp.R;
 import com.example.lawrence.weatherapp.adapters.DayAdapter;
 import com.example.lawrence.weatherapp.weather.Day;
 
-import java.lang.reflect.Array;
 import java.util.Arrays;
 
-public class DailyForecastActivity extends ListActivity {
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
+// normally we would extend ListActivity because that gives us a reference to the ListView, setListAdapter(), onListItemClick() built-in
+// we'll extend have DailyForecastActivity extend Activity so we can learn how to implement the
+// same functionality of the missing methods from scratch.
+public class DailyForecastActivity extends Activity {
 
     private Day[] mDays;
+
+    // need a reference to the ListView since we don't have it by default in extending Activity
+    // we'll do this using ButterKnife.
+    @Bind(android.R.id.list) ListView mListView;
+    // similarly we'll need a reference to the empty text view
+    @Bind(android.R.id.empty) TextView mEmptyTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_daily_forecast);
+        ButterKnife.bind(this);
 
         /*
         // using standard built-in android listview and listadapters.
@@ -58,6 +73,57 @@ public class DailyForecastActivity extends ListActivity {
         // we need a Parcelable to pass the data from MainActivity to DailyForecastActivity.
 
 
-        setListAdapter(adapter);
+        // if we extended ListActivity, we would have setListAdapter by default.
+        //setListAdapter(adapter);
+        // but since we don't, we implement the same functionality by:
+        mListView.setAdapter(adapter);
+        mListView.setEmptyView(mEmptyTextView);
+
+        mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
+                // create a simple toast when item is clicked
+                String dayOfTheWeek = mDays[position].getDayOfTheWeek();
+                String conditions = mDays[position].getSummary();
+                String highTemp = mDays[position].getTemperatureMax() + "";
+                String message = String.format(
+                        "On %s the high will be %s and it will be %s",
+                        dayOfTheWeek,
+                        conditions,
+                        highTemp
+                );
+                //Toast.makeText(this, message, Toast.LENGTH_LONG).show();
+                // can't use "this" for context since we are inside the anonymous inner class.
+                // instead use DailyForecastActivity.this to reference the outer class.
+                Toast.makeText(DailyForecastActivity.this, message, Toast.LENGTH_LONG).show();
+            }
+        });
+
     }
+
+    /*
+    // this is replaced by the anonymous inner class above.
+    // onListItemClicked() method is called when a list item is click/tapped.
+    @Override
+    protected void onListItemClick(ListView l, View v, int position, long id) {
+        // where l is ListView, v is specific item that was clicked,
+        // position is the index of item, id is optional (not set).
+
+        super.onListItemClick(l, v, position, id);
+
+        // create a simple toast when item is clicked
+        String dayOfTheWeek = mDays[position].getDayOfTheWeek();
+        String conditions = mDays[position].getSummary();
+        String highTemp = mDays[position].getTemperatureMax() + "";
+        String message = String.format(
+                "On %s the high will be %s and it will be %s",
+                dayOfTheWeek,
+                conditions,
+                highTemp
+        );
+
+        Toast.makeText(this, message, Toast.LENGTH_LONG).show();
+    }
+    */
+
 }
